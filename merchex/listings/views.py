5ca35ae616ce45ac13bd6e429ccb50d70b45c555
Didier Viret <didier.viret@orif.ch>
2024-10-24 16:07:11 +0200

    Add some URLs

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos de nous</h1><p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listing</h1><p>Afficher ici la liste des produits</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Contact</h1><p>Afficher les coordonnées de contact</p>')
