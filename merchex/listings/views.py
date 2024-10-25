ef889b5d4055a13c19875f789b40bf3027c83f3d
Didier Viret <didier.viret@orif.ch>
2024-10-25 15:02:50 +0200

    Use templates and base template for views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,17 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from listings.models import Band
+from listings.models import Band, Listing
 
 def hello(request):
     bands = Band.objects.all()
     return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
-    return HttpResponse('<h1>À propos de nous</h1><p>Nous adorons merch !</p>')
+    return render(request, 'listings/about.html')
 
 def listings(request):
-    return HttpResponse('<h1>Listing</h1><p>Afficher ici la liste des produits</p>')
+    listings = Listing.objects.all()
+    return render(request, 'listings/listings.html', {'listings': listings})
 
 def contact(request):
-    return HttpResponse('<h1>Contact</h1><p>Afficher les coordonnées de contact</p>')
+    return render(request, 'listings/contact.html')
